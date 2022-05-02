5946000229b833975d3ff522eb9dc7f298f9dbe3
aitor-arronte <aitoraalvarez@gmail.com>
2022-05-02 10:57:38 -1000

    dictionary generation

---
+++ utils/firebase.py
@@ -0,0 +1,15 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+import json
+
+cred = credentials.Certificate('creds/serviceKey.json')
+firebase_admin.initialize_app(cred)
+
+if __name__ == 'main':
+	db = firestore.client()
+	collection = db.collection('words')
+	docs = collection.get()
+	dictionary = [{'id': d._data['id'], 'word':d._data['word']} for d in docs]
+	print(json.dumps(dictionary, ensure_ascii = False), file=open('data/words.json', 'w', encoding="utf-8"))
+	print(f"Dictionary file generated with {len(dictionary)} records")
+
